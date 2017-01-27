2fd2c5bb468873f33d9f31a6621128c1a7908ba7
Jonathan Moallem <jdmoallem@gmail.com>
2017-01-27 17:40:08 +1100

    Bunch of shit

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
+  get 'forums/index'
+
+  get 'forums/show'
+
+  get 'forums/edit'
+
+  get 'forums/new'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   root 'pages#landing'

d5903cd08e4fda10316d3b43676b48bc426539a9
Jonathan Moallem <jdmoallem@gmail.com>
2017-02-07 22:43:05 +1100

    Devise login and logout work

--- app/controllers/forums_controller.rb
+++ app/controllers/forums_controller.rb
@@ -1,14 +1,15 @@
 class ForumsController < ApplicationController
-  def index
-      @forums = Forum.all
-  end
 
-  def show
-  end
+    def index
+        @forums = Forum.all
+    end
 
-  def edit
-  end
+    def show
+    end
 
-  def new
-  end
+    def edit
+    end
+
+    def new
+    end
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,9 +1,23 @@
 module ApplicationHelper
     def nav_link(link_text, page)
-      class_name = current_page?(page) ? 'active' : ''
+        class_name = current_page?(page) ? 'active' : ''
 
-      content_tag(:li, :class => class_name) do
-        link_to link_text, page
-      end
+        content_tag(:li, :class => class_name) do
+          link_to link_text, page
+        end
+    end
+
+    def bootstrap_class_for flash_type
+        { success: "alert-success", error: "alert-danger", alert: "alert-warning", notice: "alert-info" }[flash_type] || flash_type.to_s
+    end
+
+    def flash_messages(opts = {})
+        flash.each do |msg_type, message|
+            concat(content_tag(:div, message, class: "alert #{bootstrap_class_for(msg_type)} fade in") do
+                concat content_tag(:button, 'x', class: "close", data: { dismiss: 'alert' })
+                concat message
+            end)
+        end
+        nil
     end
 end

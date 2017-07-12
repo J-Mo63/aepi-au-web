5dce073e57b44ba37adaefd29232d340dd68df4d
Jonathan Moallem <jdmoallem@gmail.com>
2017-07-12 17:01:30 +1000

    Fixed bug where hex validator stopped updating and creating boards

--- app/validators/hex_validator.rb
+++ app/validators/hex_validator.rb
@@ -1,6 +1,6 @@
 class HexValidator < ActiveModel::EachValidator
 	def validate_each(object, attribute, value)
-    	unless value =~ /^#([A-Fa-f0-9]{6}|[A-Fa-f0-9]{3})$/i
+    	unless (value =~ /^#([A-Fa-f0-9]{6}|[A-Fa-f0-9]{3})$/i) || value == nil || value == ''
       		object.errors[attribute] << (options[:message] || "must be a valid CSS hex color code")
     	end
   	end

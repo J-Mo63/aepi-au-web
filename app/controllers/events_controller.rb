cc5ef4c9d086761283dfc627d366591a265ec393
Jonathan Moallem <jdmoallem@gmail.com>
2018-01-08 16:55:00 +1100

    Built out event controller

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,74 +1,70 @@
 class EventsController < ApplicationController
-  before_action :set_event, only: [:show, :edit, :update, :destroy]
-
-  # GET /events
-  # GET /events.json
   def index
-    @events = Event.all
+    load_events
   end
 
-  # GET /events/1
-  # GET /events/1.json
-  def show
-  end
-
-  # GET /events/new
   def new
-    @event = Event.new
+    build_event
   end
 
-  # GET /events/1/edit
   def edit
+  	load_event
   end
 
-  # POST /events
-  # POST /events.json
   def create
-    @event = Event.new(event_params)
-
-    respond_to do |format|
-      if @event.save
-        format.html { redirect_to @event, notice: 'Event was successfully created.' }
-        format.json { render :show, status: :created, location: @event }
-      else
-        format.html { render :new }
-        format.json { render json: @event.errors, status: :unprocessable_entity }
-      end
-    end
+    build_event
+    save_event or render 'new'
   end
 
-  # PATCH/PUT /events/1
-  # PATCH/PUT /events/1.json
   def update
-    respond_to do |format|
-      if @event.update(event_params)
-        format.html { redirect_to @event, notice: 'Event was successfully updated.' }
-        format.json { render :show, status: :ok, location: @event }
-      else
-        format.html { render :edit }
-        format.json { render json: @event.errors, status: :unprocessable_entity }
-      end
-    end
+    load_event
+    update_event or render 'edit'
   end
 
-  # DELETE /events/1
-  # DELETE /events/1.json
   def destroy
+    load_event
     @event.destroy
-    respond_to do |format|
-      format.html { redirect_to events_url, notice: 'Event was successfully destroyed.' }
-      format.json { head :no_content }
-    end
+    redirect_to events_path
+  end
+
+  def toggle_process
+    load_event
+    new_state = !@event.is_processed
+    @event.update(is_approved: new_state)
+    redirect_to events_path
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_event
-      @event = Event.find(params[:id])
-    end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
-    def event_params
-      params.require(:event).permit(:title, :itemised_budget, :runsheet, :start_time, :budget_total, :is_approved, :feedback, :expected_turnout, :user)
+  	def load_events
+  		@events = policy_scope(Event).order(created_at: :desc)
+  		@unapproved_events = @events.where(is_approved: false)
+  	end
+
+  	def load_event
+  		@event = Event.find(params[:id])
+    	authorize @event
+  	end
+
+  	def build_event
+			@event = Event.new(event_params)
+      authorize @event
+		end
+
+		def save_event
+			if @event.save
+				redirect_to events_path
+			end
+		end
+
+		def update_event
+	    if @event.update(event_params)
+				redirect_to events_path
+			end
+		end
+
+		def event_params
+    	event_params = params[:event]
+    	event_params ? reimbursement_params.permit(:title, :itemised_budget, :runsheet, :start_time, :budget_total, :is_approved, :feedback, :expected_turnout, :user) : {}
     end
 end
